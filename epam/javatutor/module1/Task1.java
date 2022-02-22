ee68d8370b3bc0f4beed764738679a390121c82d
CompS <andreichabatul@gmail.com>
2022-02-22 15:10:38 +0300

    Compete

--- epam/javatutor/module1/Task1.java
+++ epam/javatutor/module1/Task1.java
@@ -1,4 +1,27 @@
 package epam.javatutor.module1;
 
+import java.util.Scanner;
+
 public class Task1 {
+
+    /*Найдите значение функции: z = ( (a – 3 ) * b / 2) + c.*/
+
+    public static void main(String[] args) {
+        double z;
+        double a;
+        double b;
+        double c;
+
+        Scanner Scan = new Scanner(System.in);
+        System.out.print("Enter number a: ");
+        a = Scan.nextDouble();
+        System.out.print("Enter number b: ");
+        b = Scan.nextDouble();
+        System.out.print("Enter number c: ");
+        c = Scan.nextDouble();
+
+
+        z = ( (a-3) * b / 2) + c;
+        System.out.println("Result = " + z);
+    }
 }
