8d6ba74b47e51c5ffb93eeb7d32c16b30b5e2104
CompS <andreichabatul@gmail.com>
2022-02-23 10:48:47 +0300

    Compete

---
+++ epam/javatutor/module1/task8.java
@@ -0,0 +1,35 @@
+package epam.javatutor.module1;
+
+import java.util.Scanner;
+
+public class task8 {
+
+/*  Найти max{min(a, b), min(c, d)}.*/
+
+    public static void main(String[] args) {
+
+        int a;
+        int b;
+        int c;
+        int d;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter number a: ");
+        a = scan.nextInt();
+        System.out.print("Enter number b: ");
+        b = scan.nextInt();
+        System.out.print("Enter number c: ");
+        c = scan.nextInt();
+        System.out.print("Enter number d: ");
+        d = scan.nextInt();
+
+        if (Math.min(a,b) > Math.min(c,d)){
+            System.out.println("max{min(a, b), min(c, d)} = " + Math.min(a,b));
+        }
+        else {
+            System.out.println("max{min(a, b), min(c, d)} = " + Math.min(c,d));
+        }
+
+        }
+    }
