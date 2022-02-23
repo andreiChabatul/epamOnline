f5583215103499bb9c222a2cab7c95d50b5a4aa7
CompS <andreichabatul@gmail.com>
2022-02-23 09:03:21 +0300

    Compete

--- epam/javatutor/module1/task4.java
+++ epam/javatutor/module1/task4.java
@@ -1,8 +1,10 @@
 package epam.javatutor.module1;
 
-import java.math.BigDecimal;
-
 public class task4 {
+
+/*    Дано действительное число R вида nnn.ddd (три цифровых разряда в дробной и целой частях).
+    Поменять местами дробную и целую части числа и вывести полученное значение числа.*/
+
     public static void main(String[] args) {
         double r = 125.325;
         double result = (r * 1000) % 1000 + (int) r / 1000.0;
