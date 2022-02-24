36420e2a7f112670b48bea2074b25f3c1fb297b4
CompS <andreichabatul@gmail.com>
2022-02-24 13:49:58 +0300

    Compete

---
+++ epam/javatutor/module1/task20.java
@@ -0,0 +1,13 @@
+package epam.javatutor.module1;
+
+public class task20 {
+
+/*    Даны два числа. Определить цифры, входящие в запись как первого так и второго числа*/
+
+    public static void main(String[] args) {
+
+        int number1 = 10;
+        int number2 = 101;
+
+    }
+}
