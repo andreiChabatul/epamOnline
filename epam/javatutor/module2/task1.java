6e5bd12916ff1650b90815441d817ded723a2976
CompS <andreichabatul@gmail.com>
2022-02-25 08:23:08 +0300

    compete

---
+++ epam/javatutor/module2/task1.java
@@ -0,0 +1,9 @@
+package epam.javatutor.module2;
+
+public class task1 {
+
+    public static void main(String[] args) {
+        /*В массив A [N] занесены натуральные числа. Найти сумму тех элементов, которые кратны данному К.*/
+
+    }
+}
