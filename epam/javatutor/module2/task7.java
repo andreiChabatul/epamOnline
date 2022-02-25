fcd0c3758bd6e3f38f8a265463cd718ef10273d6
CompS <andreichabatul@gmail.com>
2022-02-25 10:48:53 +0300

    compete

---
+++ epam/javatutor/module2/task7.java
@@ -0,0 +1,13 @@
+package epam.javatutor.module2;
+
+public class task7 {
+
+/*    Дана последовательность целых чисел a a an  , , 1 2 . Образовать новую последовательность, выбросив из
+исходной те члены, которые равны min( , , , ) a1 a2  an.*/
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
