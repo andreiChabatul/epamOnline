e0a6f007cd8f682dcc7e818f99b2fe538a2a5515
CompS <andreichabatul@gmail.com>
2022-02-28 15:18:56 +0300

    compete

---
+++ epam/javatutor/module2/task16.java
@@ -0,0 +1,23 @@
+package epam.javatutor.module2;
+
+import java.util.Arrays;
+
+public class task16 {
+
+/*    Сформировать квадратную матрицу порядка N по правилу   */
+
+    public static void main(String[] args) {
+
+        int n = 4;
+        double [][] mas = new double [n][n];
+
+        for (int j = 0; j < mas.length; j++) {
+            for (int i = 0; i < mas.length; i++) {
+                mas[j][i] = Math.sin(((i * i) - (j * j)) / n);
+            }
+        }
+
+        System.out.println(Arrays.deepToString(mas));
+
+    }
+}
