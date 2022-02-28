84bccc5a08ce3a11f39378b4037f68c9b26d73b1
CompS <andreichabatul@gmail.com>
2022-02-28 15:25:48 +0300

    compete

--- epam/javatutor/module2/task16.java
+++ epam/javatutor/module2/task16.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 
 public class task16 {
 
-/*    Сформировать квадратную матрицу порядка N по правилу   */
+/*    Сформировать квадратную матрицу порядка N по правилу
+и подсчитать количество положительных элементов в ней   */
 
     public static void main(String[] args) {
 
@@ -12,6 +13,7 @@ public class task16 {
         double [][] mas = new double [n][n];
         double N;
         N = n;
+        int quantity = 0;
 
         for (int j = 0; j < mas.length; j++) {
             for (int i = 0; i < mas.length; i++) {
@@ -19,7 +21,16 @@ public class task16 {
             }
         }
 
+        for (double[] elem : mas) {
+            for (int i = 0; i < mas.length; i++) {
+                if (elem[i] > 0) {
+                    quantity++;
+                }
+            }
+        }
+
         System.out.println(Arrays.deepToString(mas));
+        System.out.println("The number of positive elements in the array: " + quantity);
 
     }
 }
