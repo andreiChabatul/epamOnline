a9dd565c93af64e52c4b2a7edc95272c19dac036
CompS <andreichabatul@gmail.com>
2022-02-28 21:58:02 +0300

    compete

---
+++ epam/javatutor/module2/task19.java
@@ -0,0 +1,31 @@
+package epam.javatutor.module2;
+
+import java.util.Arrays;
+
+public class task19 {
+
+/*    Найти положительные элементы главной диагонали квадратной матрицы.*/
+
+    public static void main(String[] args) {
+
+        int n = (int) (Math.random() * 10);
+        int[][] mas = new int[n][n];
+        int a = 0;
+
+        for (int j = 0; j < mas.length; j++) {
+            for (int i = 0; i < mas.length; i++) {
+                mas[j][i] = -500 + (int) (Math.random() * 1000);
+            }
+        }
+        System.out.println(Arrays.deepToString(mas));
+
+
+        for (int[] elem : mas) {
+            if (elem[a] > 0) {
+                System.out.print(elem[a] + " ");
+            }
+            a++;
+        }
+
+    }
+}
