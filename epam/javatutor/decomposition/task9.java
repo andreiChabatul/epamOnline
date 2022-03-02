ac128c441ca9f77daface08e835abe017e70d594
CompS <andreichabatul@gmail.com>
2022-03-02 13:56:19 +0300

    compete

--- epam/javatutor/decomposition/task9.java
+++ epam/javatutor/decomposition/task9.java
@@ -7,7 +7,6 @@ public class task9 {
 /*    Даны числа X, Y, Z, Т — длины сторон четырехугольника. Написать метод(методы) вычисления его площади,
     если угол между сторонами длиной X и Y— прямой.*/
 
-
     public static void main(String[] args) {
 
         int x;
@@ -26,20 +25,20 @@ public class task9 {
 
         if (x < 0 || x ==0 ||y < 0 || y ==0 || z < 0 || z ==0 || t < 0 || t ==0
                 || calculationAngle(x, y, z, t) < 0 || calculationAngle(x, y, z, t) == 0){
-            System.out.println("eq");
+            System.out.println("Error enter");
         } else {
-            System.out.println(calculationAngle(x, y, z, t));
-            System.out.println(calculationarea(calculationAngle(x, y, z, t), x, y, z, t));
+            System.out.println("Area is: " + calculationarea(calculationAngle(x, y, z, t), x, y, z, t));
         }
     }
 
     public static double calculationAngle (int a, int b, int c, int d){
         double hypotenuse;
         double result;
-        hypotenuse = Math.sqrt((a*a)+(b*b));
 
+        hypotenuse = Math.sqrt((a*a)+(b*b));
         result = Math.acos(((c*c)+(d*d)-(hypotenuse*hypotenuse))/(2*c*d));
         result = Math.toDegrees(result);
+
         return result;
     }
 
@@ -49,14 +48,17 @@ public class task9 {
 
         perimeter = (a+b+c+d)/2;
         result = Math.sqrt(((perimeter-a)*(perimeter-b)*(perimeter-c)*(perimeter-d))
-                -(a*b*c*d*(Math.cos((e+90)/2))*Math.cos((e+90)/2)));
+                -(a*b*c*d*(Math.cos(Math.toRadians(90+e)/2))*Math.cos(Math.toRadians(90+e)/2)));
+
         return result;
     }
 
     public static int enter (){
         int x;
+
         Scanner scan = new Scanner(System.in);
         x = scan.nextInt();
+
         return x;
     }
 }
