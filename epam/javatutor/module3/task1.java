c68a528c71cf05179f14d482da9a0be2fcf6dfb5
CompS <andreichabatul@gmail.com>
2022-03-31 10:55:05 +0300

    compete

---
+++ epam/javatutor/module3/task1.java
@@ -0,0 +1,28 @@
+package epam.javatutor.module3;
+
+import java.util.Arrays;
+
+public class task1 {
+    /*Дан массив названий переменных в camelCase. Преобразовать названия в snake_case.*/
+
+    public static void main(String[] args) {
+
+        String[] str = {"infixCaps", "capWords", "camelCase", "wordCase", "nerdСaps", "polyCaps", "bumpyCaps", "humpBack", "camelCaps"};
+        String[] resultStr = new String [str.length];
+
+        for (int j = 0; j < str.length; j++) {
+            for (int i = 0; i < str[j].length() - 1; i++) {
+                if (Character.isLowerCase(str[j].charAt(i)) && Character.isUpperCase(str[j].charAt(i + 1))) {
+                    String oneWord = str[j].substring(0, i + 1);
+                    String secondWord = str[j].substring(i + 1);
+                    secondWord = secondWord.replace(secondWord.charAt(0), Character.toLowerCase(secondWord.charAt(0)));
+                    resultStr[j] = oneWord + "_" + secondWord;
+                }
+            }
+        }
+        System.out.print("Starting array: ");
+        System.out.println(Arrays.toString(str));
+        System.out.print("Modified array: ");
+        System.out.print(Arrays.toString(resultStr));
+    }
+}
