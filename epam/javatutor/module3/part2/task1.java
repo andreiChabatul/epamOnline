b20e96a45a79960131e8b3f0f88972c53db11cb9
CompS <andreichabatul@gmail.com>
2022-03-31 13:59:50 +0300

    compete

---
+++ epam/javatutor/module3/part2/task1.java
@@ -0,0 +1,8 @@
+package epam.javatutor.module3.part2;
+
+public class task1 {
+    /*Дан текст (строка). Найдите наибольшее количество подряд идущих пробелов в нем.*/
+    public static void main(String[] args) {
+
+    }
+}
